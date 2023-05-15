e880cc097b2b4687ec08156ab196d8b53baa6580
elhajjaji.riad <riad@gmail.com>
2023-05-15 04:08:02 -0400

    +

--- src/routes/api/paymentRoutes.ts
+++ src/routes/api/paymentRoutes.ts
@@ -5,28 +5,55 @@ import axios from 'axios'
 
 const router = express.Router();
 
-const ordersServiceUrl="http://localhost:5002"
+const ordersServiceUrl="http://localhost:5002/api"
 
 router.post("/", async (req: Request, res: Response) => {
-  try {
-    const paymentExisted = await Payment.find({ orderId: req.body.orderId });
-    if (paymentExisted[0]) {
-      return res.json({ message: "Order is already paid" }).end();
-    }
-    const newPayment = new Payment({
-      ...req.body,
-    });
-    if (!newPayment) {
-      throw new Error("Payment failed");
-    }
-    await newPayment.save();
-    // mark order as paid order
-    await axios.put(`${ordersServiceUrl}/orders/${req.body.orderId}`);
 
-    return res.json(newPayment).end();
-  } catch (error:any) {
-      return res.status(500).json({ message: "Internal server error" }).end();
+  try {
+    // Process the payment logic using the payment details from req.body
+    // const paymentResult = await processPayment(req.body);
+  const  { orderId,amount,cardNumber}=req.body
+    const newPayment = new Payment({
+      orderId,amount,cardNumber
+    })
+    // If payment is successful, mark the order as paid
+    // if (newPayment) {
+      await axios.put(`${ordersServiceUrl}/orders/${req.body.orderId}`);
+    // }
+    // Send the payment result back to the gateway
+    res.json(newPayment);
+  } catch (error) {
+    // Handle errors and send an appropriate response
+    res.status(500).json({ error: 'Failed to process payment' });
   }
+
+
+
+
+  // try {
+    // const paymentExisted = await Payment.find({ orderId: req.body.orderId });
+    // if (paymentExisted) {
+    //   return res.json({ message: "Order is already paid" }).end();
+    // }
+  //   const  { orderId,amount,cardNumber}=req.body
+  //   const newPayment = new Payment({
+  //     orderId,amount,cardNumber
+  //   }
+    
+  //   );
+  //   // if (!newPayment) {
+  //   //   throw new Error("Payment failed");
+  //   // }
+  //   await newPayment.save();
+  //   // mark order as paid order
+  //   console.log("before order payed")
+  //   await axios.put(`${ordersServiceUrl}/api/orders/${req.body.orderId}`,req.body).then(()=>console.log("order payed"));
+      
+  //   return res.json(newPayment);
+  // } catch (error:any) {
+  //   console.log("error in payment")
+  //     return res.status(500).json({ message: "Internal server error" }).end();
+  // }
 });
 
 // export = router;
