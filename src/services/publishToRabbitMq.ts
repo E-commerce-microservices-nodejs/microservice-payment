daff83de58460b57a5aadda203a5d917dc0bfc55
elhajjaji.riad <riad@gmail.com>
2023-06-04 16:35:16 -0400

    ++

--- src/services/publishToRabbitMq.ts
+++ src/services/publishToRabbitMq.ts
@@ -1,29 +1,35 @@
-import amqp from 'amqplib'
-import dotenv from 'dotenv'
-dotenv.config()
-export default async function sendEmail(customer_email:string,customer_fullname:string,amount:number,subject:string,orderId:string,transactionId:string) {
-    const msgBuffer = Buffer.from(
-      JSON.stringify({
-        to: customer_email,
-        subject: subject,
-        fullname:customer_fullname,
-        amount,
-        orderId,
-        transactionId
+/* eslint-disable no-console */
+import amqp from 'amqplib';
+import dotenv from 'dotenv';
 
-  
-      })
-    );
-    try {
-      const connection = await amqp.connect(`${process.env.RABBIT_MQ_URI}`);
-      const channel = await connection.createChannel();
-      await channel.assertQueue("email");
-      channel.sendToQueue("email", msgBuffer);
-      console.log("Sending message to email queue");
-      await channel.close();
-      await connection.close();
-    } catch (ex) {
-      console.error(ex);
-    }
+dotenv.config();
+export default async function sendEmail(
+  customerEmail: string,
+  customerFullname: string,
+  amount: number,
+  subject: string,
+  orderId: string,
+  transactionId: string
+): Promise<void> {
+  const msgBuffer = Buffer.from(
+    JSON.stringify({
+      to: customerEmail,
+      subject,
+      fullname: customerFullname,
+      amount,
+      orderId,
+      transactionId,
+    })
+  );
+  try {
+    const connection = await amqp.connect(`${process.env.RABBIT_MQ_URI}`);
+    const channel = await connection.createChannel();
+    await channel.assertQueue('email');
+    channel.sendToQueue('email', msgBuffer);
+    console.log('Sending message to email queue');
+    await channel.close();
+    await connection.close();
+  } catch (ex) {
+    console.error(ex);
   }
-  
+}
